9cab8f7a3f8428bb46e039c8ee5170a660e2dca3
Dmytro.Sch <dmytro.sch@list.ru>
2021-02-07 21:29:36 +0200

    path

--- server_html.js
+++ server_html.js
@@ -23,6 +23,6 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("static"));
+app.use("/*", express.static("static"));
 
 app.listen(8080, () => console.log("Server listen on 8080 port"));
