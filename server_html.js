908d5302097ae818accfe2ed231ab6ea28932c05
Dmytro.Sch <dmytro.sch@list.ru>
2021-02-07 22:10:39 +0200

    heroku deploy

--- server_html.js
+++ server_html.js
@@ -1,25 +1,3 @@
-// var static = require("node-static");
-
-// var file = new static.Server("./static");
-
-// require("http")
-//     .createServer(function (request, response) {
-//         request
-//             .addListener("end", function () {
-//                 // console.log(request.headers);
-
-//                 request.url = "/";
-//                 // console.log(request.url);
-//                 file.serve(request, response, (e, res) => {
-//                     console.log("in file: ", e, res);
-//                 });
-//             })
-//             .resume();
-//     })
-//     .listen(8080);
-
-///////////////////////
-
 const express = require("express");
 const app = express();
 
